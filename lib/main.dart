3f5f8185a5b9e19c6b90a2518490abdf17eb0c7f
kp0430 <122626179+kp0430@users.noreply.github.com>
2025-09-18 21:45:17 -0400

    add dynamic pet color and mood indicator

--- lib/main.dart
+++ lib/main.dart
@@ -1,85 +1,119 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter/material.dart';
-
 void main() {
-runApp(MaterialApp(
-home: DigitalPetApp(),
-));
+  runApp(MaterialApp(
+    home: DigitalPetApp(),
+  ));
 }
+
 class DigitalPetApp extends StatefulWidget {
-@override
-_DigitalPetAppState createState() => _DigitalPetAppState();
+  @override
+  _DigitalPetAppState createState() => _DigitalPetAppState();
 }
+
 class _DigitalPetAppState extends State<DigitalPetApp> {
-String petName = "Your Pet";
-int happinessLevel = 50;
-int hungerLevel = 50;
-void _playWithPet() {
-setState(() {
-happinessLevel += 10;
-_updateHunger();
-});
-}
-void _feedPet() {
-setState(() {
-hungerLevel -= 10;
-_updateHappiness();
-});
-}
-void _updateHappiness() {
-if (hungerLevel < 30) {
-happinessLevel -= 20;
-} else {
-happinessLevel += 10;
-}
-}
-void _updateHunger() {
-setState(() {
-hungerLevel += 5;
-if (hungerLevel > 100) {
-hungerLevel = 100;
-happinessLevel -= 20;
-}
-});
-}
-@override
-Widget build(BuildContext context) {
-return Scaffold(
-appBar: AppBar(
-title: Text('Digital Pet'),
-),
-body: Center(
-child: Column(
-mainAxisAlignment: MainAxisAlignment.center,
-children: <Widget>[
-Text(
-'Name: $petName',
-style: TextStyle(fontSize: 20.0),
-),
-SizedBox(height: 16.0),
-Text(
-'Happiness Level: $happinessLevel',
-style: TextStyle(fontSize: 20.0),
-),
-SizedBox(height: 16.0),
-Text(
-'Hunger Level: $hungerLevel',
-style: TextStyle(fontSize: 20.0),
-),
-SizedBox(height: 32.0),
-ElevatedButton(
-onPressed: _playWithPet,
-child: Text('Play with Your Pet'),
-),
-SizedBox(height: 16.0),
-ElevatedButton(
-onPressed: _feedPet,
-child: Text('Feed Your Pet'),
-),
-],
-),
-),
-);
-}
+  String petName = "Your Pet";
+  int happinessLevel = 50;
+  int hungerLevel = 50;
+  Color petColor = Colors.yellow; 
+  String mood = "Neutral"; 
+
+  void _playWithPet() {
+    setState(() {
+      happinessLevel += 10;
+      _updateHunger();
+      _updatePetColorAndMood(); 
+    });
+  }
+
+  void _feedPet() {
+    setState(() {
+      hungerLevel -= 10;
+      _updateHappiness();
+      _updatePetColorAndMood();
+    });
+  }
+
+  void _updateHappiness() {
+    if (hungerLevel < 30) {
+      happinessLevel -= 20;
+    } else {
+      happinessLevel += 10;
+    }
+  }
+
+  void _updateHunger() {
+    setState(() {
+      hungerLevel += 5;
+      if (hungerLevel > 100) {
+        hungerLevel = 100;
+        happinessLevel -= 20;
+      }
+    });
+  }
+
+  void _updatePetColorAndMood() {
+    if (happinessLevel > 70) {
+      petColor = Colors.green;
+      mood = "Happy";
+    } else if (happinessLevel >= 30) {
+      petColor = Colors.yellow;
+      mood = "Neutral";
+    } else {
+      petColor = Colors.red;
+      mood = "Unhappy";
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Digital Pet'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Icon(
+              Icons.catching_pokemon,
+              size: 100,
+              color: petColor,
+            ),
+            SizedBox(height: 16.0),
+            Text(
+              'Name: $petName',
+              style: TextStyle(fontSize: 20.0),
+            ),
+            
+            SizedBox(height: 8.0),
+            Text(
+              'Mood: $mood',
+              style: TextStyle(fontSize: 18.0),
+            ),
+            SizedBox(height: 16.0),
+            Text(
+              'Happiness Level: $happinessLevel',
+              style: TextStyle(fontSize: 20.0),
+            ),
+            SizedBox(height: 16.0),
+            Text(
+              'Hunger Level: $hungerLevel',
+              style: TextStyle(fontSize: 20.0),
+            ),
+            SizedBox(height: 32.0),
+            ElevatedButton(
+              onPressed: _playWithPet,
+              child: Text('Play with Your Pet'),
+            ),
+            SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: _feedPet,
+              child: Text('Feed Your Pet'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
